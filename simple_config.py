59589a8b3ebc4f8e77f5bd8aea4e8fabceb24c0a
Jerry Hogsett <jhogsett@gmail.com>
2023-02-04 14:35:53 -0800

    simple class to handle YAML config

---
+++ simple_config.py
@@ -0,0 +1,15 @@
+import yaml
+
+class SimpleConfig:
+    def __new__(cls, path : str = "config.yaml"):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SimpleConfig, cls).__new__(cls)
+            cls.instance.init(path)
+        return cls.instance
+
+    def init(self, path : str):
+        with open(path) as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+
+    def get(self, key : str):
+        return self.config[key]
